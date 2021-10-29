615e040cd648754d328806f70bcb5b8cd63773b2
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2021-10-28 21:04:04 -0300

    adiconado o healthcheck

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router, Response, Request, NextFunction } from "express";
 import { authRouter } from './auth.router'
 import { bookRouter } from "./book.router";
 
@@ -6,3 +6,7 @@ export const router = Router();
 
 router.use("/auth", authRouter)
 router.use("/book", bookRouter)
+
+router.get('/health-check', (_request: Request, response: Response, _next: NextFunction) => {
+    return response.status(200).json({message: "server working"})
+})
